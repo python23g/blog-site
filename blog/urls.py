e61432195a2160319eef4d6f0f676a6e491164af
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-02 18:01:46 +0500

    blog page

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
     path('contact/', views.ContactView.as_view(), name='contact'),
     path('blogs/', views.BlogsView.as_view(), name='blogs'),
+    path('blogs/<int:pk>/', views.BlogsView.as_view(), name='blogs_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpRequest, HttpResponse
+from .models import Blog
 
 
 class HomeView(View):
@@ -19,5 +20,12 @@ class ContactView(View):
 
 
 class BlogsView(View):
-    def get(self, request: HttpRequest) -> HttpResponse:
-        return render(request, 'blogs.html')
+    def get(self, request: HttpRequest, pk=None) -> HttpResponse:
+        if pk is None:
+            context = {
+                'blogs': [{'id': blog.id, 'title': blog.title, 'content': blog.content[:100], 'published': blog.published} for blog in Blog.objects.all()]
+            }
+            return render(request, 'blogs.html', context=context)
+        else:
+            blog = Blog.objects.get(id=pk)
+            return render(request, 'blog_detail.html', context={'blog': blog})
