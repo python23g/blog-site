92ddab01f36378d64da8282453d6065a118c69a0
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-02 17:30:51 +0500

    base.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.HomeView.as_view()),
-    path('about/', views.AboutView.as_view()),
-    path('contact/', views.ContactView.as_view()),
-    path('blogs/', views.BlogsView.as_view()),
+    path('', views.HomeView.as_view(), name='home'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('blogs/', views.BlogsView.as_view(), name='blogs'),
 ]
